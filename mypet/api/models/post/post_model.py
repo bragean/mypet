178d7c7683949ee6d58550a2207ced9b0d307a7e
bragean-gett <140440637+bragean-gett@users.noreply.github.com>
2025-01-21 16:15:28 -0500

    new model

---
+++ mypet/api/models/post/post_model.py
@@ -0,0 +1,25 @@
+from django.db.models import (
+    CharField,
+    ImageField,
+    ForeignKey,
+    CASCADE,
+    SET_NULL
+)
+from ..base.base_model import Base
+from ..pet.pet_model import Pet
+from ..location.general_model import District
+from ..location.point_model import Point
+
+class Post(Base):
+    title = CharField(max_length=100)
+    description = CharField(max_length=255)
+    pet = ForeignKey(Pet, on_delete=CASCADE)
+    district = ForeignKey(District, on_delete=SET_NULL, null=True)
+    point = ForeignKey(Point, on_delete=SET_NULL, null=True)
+    
+    
+
+
+class ImagePost(Base):
+    name = CharField(max_length=32, blank=True)
+    image = ImageField(upload_to="post/", null=True, blank=True)
